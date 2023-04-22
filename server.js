9c6b4c47ed3d5d5b0d9fbf4aac109208e6fec5db
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-04-22 12:03:34 -0500

    render landing page at /

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 require("dotenv").config()
+const es6Renderer = require("express-es6-template-engine")
 const express = require("express")
+
 
 const server = express()
 const PORT = process.env.PORT || 8080
+
+server.engine('html', es6Renderer);
+server.set('views', 'views');
+server.set('view engine', 'html');
+
+server.get("/", (req, res) => {
+    res.render("index")
+})
 
 server.get("/heartbeat", (req, res) => {
     res.json({
