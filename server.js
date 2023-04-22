4779c263f53b9c44decb7da0b4dc2063dc32ee84
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-04-22 09:49:42 -0500

    add server with /heartbeat endpoint

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-//  SERVER
+const express = require("express")
+
+const server = express()
+const PORT = 8080
+
+server.get("/heartbeat", (req, res) => {
+    res.json({
+        "is" : "working"
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`The server is running at PORT ${PORT}`)
+})
