a5fdb9279fcb384bcc017c2b0a1b2df630cb5f1e
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-04-23 23:03:23 -0500

    style active nav

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
 const es6Renderer = require("express-es6-template-engine")
-const { setMainView } = require("./utils")
+const { setMainView, setNavs } = require("./utils")
 const express = require("express")
 const navs = require("./data/navs.json")
 
@@ -16,49 +16,50 @@ server.use(express.static(__dirname + '/public'))
 
 server.get("/", (req, res) => {
     res.render("index", {
-        locals: {navs},
+        locals: setNavs(req.url, navs),
         partials: setMainView("landing")
     })
 })
 
 server.get("/about", (req, res) => {
+    console.log("pota", req.url)
     res.render("index", {
-        locals: {navs},
+        locals: setNavs(req.url, navs),
         partials: setMainView("about")
     })
 })
 
 server.get("/contact-us", (req, res) => {
     res.render("index", {
-        locals: {navs},
+        locals: setNavs(req.url, navs),
         partials: setMainView("contact-us")
     })
 })
 
 server.get("/gallery", (req, res) => {
     res.render("index", {
-        locals: {navs},
+        locals: setNavs(req.url, navs),
         partials: setMainView("gallery")
     })
 })
 
 server.get("/login", (req, res) => {
     res.render("index", {
-        locals: {navs},
+        locals: setNavs(req.url, navs),
         partials: setMainView("login")
     })
 })
 
 server.get("/logout", (req, res) => {
     res.render("index", {
-        locals: {navs},
+        locals: setNavs(req.url, navs),
         partials: setMainView("logout")
     })
 })
 
 server.get("/profile", (req, res) => {
     res.render("index", {
-        locals: {navs},
+        locals: setNavs(req.url, navs),
         partials: setMainView("profile")
     })
 })
--- utils/index.js
+++ utils/index.js
@@ -6,4 +6,15 @@ function setMainView(view){
     }
 }
 
-module.exports = { setMainView }
+function setNavs(currentHref, navs) {
+    const _navs = navs.map(nav => {
+        nav.className = "";
+        if(nav.href === currentHref){
+            nav.className = "active"
+        }
+        return nav
+    })
+    return {navs}
+}
+
+module.exports = { setMainView, setNavs }
