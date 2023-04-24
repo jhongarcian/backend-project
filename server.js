1840d456dc221d6ba1475eafcdaaae8b8ff1ba7e
John Garcia <98444386+jhongarcian@users.noreply.github.com>
2023-04-23 22:32:37 -0500

    add nav items

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const es6Renderer = require("express-es6-template-engine")
 const { setMainView } = require("./utils")
 const express = require("express")
+const navs = require("./data/navs.json")
 
 
 const server = express()
@@ -15,12 +16,14 @@ server.use(express.static(__dirname + '/public'))
 
 server.get("/", (req, res) => {
     res.render("index", {
+        locals: {navs},
         partials: setMainView("landing")
     })
 })
 
 server.get("/login", (req, res) => {
     res.render("index", {
+        locals: {navs},
         partials: setMainView("login")
     })
 })
